85b3418eb1aff52667b742cc3f23321c0e40919a
hongkong792 <hongkong792@163.com>
2017-01-24 12:26:38 +0800

    完善

--- 百思不解/百思不解/modules/Essence-精华/Controller/XMGTopicViewController.m
+++ 百思不解/百思不解/modules/Essence-精华/Controller/XMGTopicViewController.m
@@ -13,6 +13,7 @@
 #import "XMGTopic.h"
 #import <MJExtension.h>
 #import <MJRefresh.h>
+#import <AFNetworking.h>
 @interface XMGTopicViewController ()
 
 /**帖子数据*/
@@ -21,6 +22,8 @@
 @property (nonatomic,assign)NSInteger page;
 /**上一次请求的参数*/
 @property (nonatomic,strong)NSDictionary *params;
+/** 当加载下一页数据时需要这个参数 */
+@property (nonatomic, copy) NSString *maxtime;
 @end
 
 @implementation XMGTopicViewController
@@ -45,6 +48,112 @@ static NSString * const topicCell = @"topicCell";
 }
 
 
+
+/**
+ *添加刷新控件
+ */
+- (void)setupRefresh
+{
+    self.tableView.header = [MJRefreshHeader headerWithRefreshingTarget:self refreshingAction:@selector(loadNewTopics)];
+    self.tableView.header.autoChangeAlpha = YES;
+    [self.tableView.header beginRefreshing];
+    self.tableView.footer = [MJRefreshFooter footerWithRefreshingTarget:self refreshingAction:@selector(loadMoreTopics)];
+    
+    
+}
+
+#pragma mark - 数据处理
+/**
+ * 加载新的帖子数据
+ */
+- (void)loadNewTopics
+{
+    // 结束上啦
+    [self.tableView.footer endRefreshing];
+    
+    // 参数
+    NSMutableDictionary *params = [NSMutableDictionary dictionary];
+    params[@"a"] = @"list";
+    params[@"c"] = @"data";
+    params[@"type"] = @(self.type);
+    self.params = params;
+    
+    // 发送请求
+    [[AFHTTPSessionManager manager] GET:@"http://api.budejie.com/api/api_open.php" parameters:params success:^(NSURLSessionDataTask *task, NSDictionary *responseObject) {
+        if (self.params != params) return;
+        
+        // 存储maxtime
+        self.maxtime = responseObject[@"info"][@"maxtime"];
+        
+        // 字典 -> 模型
+        self.topics = [XMGTopic objectArrayWithKeyValuesArray:responseObject[@"list"]];
+        
+        // 刷新表格
+        [self.tableView reloadData];
+        
+        // 结束刷新
+        [self.tableView.header endRefreshing];
+        
+        // 清空页码
+        self.page = 0;
+    } failure:^(NSURLSessionDataTask *task, NSError *error) {
+        if (self.params != params) return;
+        
+        // 结束刷新
+        [self.tableView.header endRefreshing];
+    }];
+}
+
+// 先下拉刷新, 再上拉刷新第5页数据
+
+// 下拉刷新成功回来: 只有一页数据, page == 0
+// 上啦刷新成功回来: 最前面那页 + 第5页数据
+
+/**
+ * 加载更多的帖子数据
+ */
+- (void)loadMoreTopics
+{
+    // 结束下拉
+    [self.tableView.header endRefreshing];
+    
+    // 参数
+    NSMutableDictionary *params = [NSMutableDictionary dictionary];
+    params[@"a"] = @"list";
+    params[@"c"] = @"data";
+    params[@"type"] = @(self.type);
+    NSInteger page = self.page + 1;
+    params[@"page"] = @(page);
+    params[@"maxtime"] = self.maxtime;
+    self.params = params;
+    
+    // 发送请求
+    [[AFHTTPSessionManager manager] GET:@"http://api.budejie.com/api/api_open.php" parameters:params success:^(NSURLSessionDataTask *task, NSDictionary *responseObject) {
+        if (self.params != params) return;
+        
+        // 存储maxtime
+        self.maxtime = responseObject[@"info"][@"maxtime"];
+        
+        // 字典 -> 模型
+        NSArray *newTopics = [XMGTopic objectArrayWithKeyValuesArray:responseObject[@"list"]];
+        [self.topics addObjectsFromArray:newTopics];
+        
+        // 刷新表格
+        [self.tableView reloadData];
+        
+        // 结束刷新
+        [self.tableView.footer endRefreshing];
+        
+        // 设置页码
+        self.page = page;
+    } failure:^(NSURLSessionDataTask *task, NSError *error) {
+        if (self.params != params) return;
+        
+        // 结束刷新
+        [self.tableView.footer endRefreshing];
+    }];
+}
+
 /**
  *初始化表格
  */
@@ -66,7 +175,6 @@ static NSString * const topicCell = @"topicCell";
 {
     XMGTopic * topic = self.topics[indexPath.row];
     return topic.height;
-    
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
